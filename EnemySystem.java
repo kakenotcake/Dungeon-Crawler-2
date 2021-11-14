fe95955215d23d4576e10f420ab060aee0300883
Nahom Tilahun <ntilahun@mail.umw.edu>
2021-11-14 23:40:06 +0000

    readded position

--- EnemySystem.java
+++ EnemySystem.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class EnemySystem{
     private int EnemyHealth;
     private ArrayList<Enemy> enemies;
+    private Position EnemyPosition;
 
 
     //hydration constructor to save information of Enemies
@@ -11,6 +12,7 @@ public class EnemySystem{
         this.enemies=new ArrayList<Enemy>();
         Scanner stdin = new Scanner(new FileReader(fileName));
         this.EnemyHealth=stdin.nextInt();
+	this.EnemyPosition=s.nextLine();
         try{
             while(true){
                 Enemy a=new Enemy(stdin);
@@ -21,6 +23,7 @@ public class EnemySystem{
         }catch (NoMoreEnemiesException e){
 
         }
+	s.nextLine();
 
 
     }
