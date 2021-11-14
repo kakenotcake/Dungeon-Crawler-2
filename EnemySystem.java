ba7730dab6fd790b4e4322b931dc53661b615c6c
Nahom Tilahun <ntilahun@mail.umw.edu>
2021-11-14 23:19:38 +0000

    fixed exception error for EnemySystem

--- EnemySystem.java
+++ EnemySystem.java
@@ -1,16 +1,31 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class EnemySystem{
-	private int EnemyHealth;
-	private ArrayList<Enemy>enemies;
-	private Position EnemyPosition;
-
-	//hydration constructor to save information of Enemies
-	EnemySystem(String fileName){
-		this.enemies=new ArrayList<Enemy>();
-		Scanner s = new Scanner(new FileReader(filename));
-		this.EnemyHealth=s.nextInt();
+    private int EnemyHealth;
+    private ArrayList<Enemy> enemies;
 
 
-	}
+    //hydration constructor to save information of Enemies
+    EnemySystem(String fileName)throws Exception {
+        this.enemies=new ArrayList<Enemy>();
+        Scanner stdin = new Scanner(new FileReader(fileName));
+        this.EnemyHealth=stdin.nextInt();
+        try{
+            while(true){
+                Enemy a=new Enemy(stdin);
+                enemies.add(a);
+            }
+
+
+        }catch (NoMoreEnemiesException e){
+
+        }
+
+
+    }
 
 
 }
+
+
