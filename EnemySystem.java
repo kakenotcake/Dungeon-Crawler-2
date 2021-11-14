22c7f12932aaa5f84300d5df781b7233d2953975
Nahom Tilahun <ntilahun@mail.umw.edu>
2021-11-14 05:30:35 +0000

    added constructor for EnemySystem

--- EnemySystem.java
+++ EnemySystem.java
@@ -1,6 +1,16 @@
 public class EnemySystem{
 	private int EnemyHealth;
 	private ArrayList<Enemy>enemies;
+	private Position EnemyPosition;
+
+	//hydration constructor to save information of Enemies
+	EnemySystem(String fileName){
+		this.enemies=new ArrayList<Enemy>();
+		Scanner s = new Scanner(new FileReader(filename));
+		this.EnemyHealth=s.nextInt();
+
+
+	}
 
 
 }
