29ae4ccb112b4aa44f9fde4502c2cdb1f9e57d5e
Nahom Tilahun <ntilahun@mail.umw.edu>
2021-11-16 03:50:30 +0000

    got rid of the catch

--- EnemySystem.java
+++ EnemySystem.java
@@ -1,25 +1,22 @@
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class EnemySystem{
     private int EnemyHealth;
     private ArrayList<Enemy> enemies;
-    private Position EnemyPosition;
-     //hydration constructor to save information of Enemies
-    EnemySystem(String fileName)throws Exception {
+
+
+
+    //hydration constructor to save information of Enemies
+    EnemySystem(String fileName)throws Exception, NoMoreEnemiesException {
         this.enemies=new ArrayList<Enemy>();
         Scanner stdin = new Scanner(new FileReader(fileName));
         this.EnemyHealth=stdin.nextInt();
         //this.EnemyPosition=s.nextLine();
-        try{
-            while(true){
-                Enemy a = new Enemy(stdin);
-                enemies.add(a);
-            }
-
-
-        }catch (NoMoreEnemiesException e){
-
+        while(true){
+            Enemy a = new Enemy(stdin);
+            enemies.add(a);
         }
 
 
@@ -27,3 +24,4 @@ public class EnemySystem{
 
 
 }
+
