419c10b75d65e1d19bc6916dbaf8e352495288df
Kelsey <kepviolin@gmail.com>
2021-12-06 23:34:54 +0000

    Item javaDoc!!

--- Item.java
+++ Item.java
@@ -1,5 +1,7 @@
-/**
-/ * This class is responsible for managing Item objects and their appropriate methods.
+/** This class is responsible for managing Item objects and their appropriate methods.
+ *
+ * @author Ian Finlayson, Nahom Tilahun, Katelyn Hernandez, Kelsey Payne
+ * @version 2021
  */
 public class Item {
 
@@ -42,7 +44,9 @@ public class Item {
 	}
 
         /**
-         * A getter. This method returns the weight of the Item being referred to.
+         * A getter- this method returns the weight of the Item being referred to.
+	 *
+	 * @return the int representing the Item's weight
          */
         public int getWeight()
         {
@@ -50,7 +54,9 @@ public class Item {
         }
 
         /**
-         * A getter. This method returns the value of the Item being referred to.
+         * A getter- this method returns the value of the Item being referred to.
+	 *
+	 * @return the int representing the Item's value
          */
         public int getValue()
         {
@@ -58,7 +64,9 @@ public class Item {
         }
 
         /**
-         * A getter. This method returns the name of the Item being referred to.
+         * A getter- this method returns the name of the Item being referred to.
+	 *
+	 * @return the String name of an Item
          */
         public String getName()
         {
@@ -66,41 +74,57 @@ public class Item {
         }
 
         /**
-         * A getter. This method returns the Type (enumeration) of the Item being referred to.
+         * A getter- this method returns the Type (enumeration) of the Item being referred to.
+	 *
+	 * @return the enum ItemType of an Item
          */
         public ItemType getType()
         {
                 return this.type;
         }
 
+        /**
+	 * A getter- this method returns the strength of the Item being referred to.
+	 *
+	 * @return the int representing an Item's strength
+	 */
 	public int getStrength()
 	{
 		return this.strength;
 	}
-	/**This method gets the class that the player chose at the begining of the game
-	 *@return the class of the player 
+
+	/**This method gets the class that the player chose at the begining of the game.
 	 *
+	 *@return the class of the player
 	 */
 	public PlayerClass getPlayerClass()
 	{
 		return this.playerClass;
 	}
-	/**This method get the commentary that occurs when an action occurs
-	 *@return the string containing the commentary based on an action occuring
+
+	/**This method gets the commentary that occurs when an action occurs.
 	 *
+	 *@return the string containing the commentary based on an action occurring
 	 */
 	public String getCommentary()
 	{
 		return this.commentary;
 	}
 
-        /**
-         * The toString() method for Item objects. This method is responsible for returning all of the parameters of an Item object in a legible way.
+        /** This method is responsible for returning all of the properties of an Item object
+	 * in a legible way.
+	 *
+	 * @return a String containing the Item's properies
          */
 	public String toString()
 	{
 		return this.type + " " + this.name + " " + this.weight + " " +  this.value + " " + this.strength + " " + this.playerClass;
 	}
+
+	/**This method is responsible for returning all of the properties of an Item object when the game is saved.
+	 *
+	 * @return a String containing the saved Item's properies
+	 */
         public String toStringForSave()
         {
                 return this.type + "\n" + this.name + "\n" + this.weight + "\n" + this.value + "\n" + this.strength + "\n" + this.playerClass + "\n";
